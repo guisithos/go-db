74f6103c6aa09551033623851be6d824f89b8b10
guisithos <gui.thomas@outlook.com>
2024-04-15 14:27:42 -0300

    Modified env

--- database/connection.go
+++ database/connection.go
@@ -3,17 +3,32 @@ package database
 import (
 	"database/sql"
 	"fmt"
+	"log"
+	"os"
+	"strconv"
 
-	"github.com/guisithos/go-db/env"
+	"github.com/joho/godotenv"
+
 	go_ora "github.com/sijms/go-ora/v2"
 )
 
 func ConnectToDatabase() (*sql.DB, error) {
-	user := env.LoadEnviroment("DB_USER")
-	password := env.LoadEnviroment("DB_PASSWORD")
-	host := env.LoadEnviroment("DB_HOST")
-	port := env.LoadEnviroment("DB_PORT")
-	serviceName := env.LoadEnviroment("DB_SERVICE_NAME")
+
+	err := godotenv.Load(".env")
+
+	if err != nil {
+		log.Fatalf("Error loading .env file")
+	}
+
+	user := os.Getenv("DB_USER")
+	password := os.Getenv("DB_PASSWORD")
+	host := os.Getenv("DB_HOST")
+	portStr := os.Getenv("DB_PORT")
+	port, err := strconv.Atoi(portStr)
+	if err != nil {
+		return nil, err
+	}
+	serviceName := os.Getenv("DB_SERVICE_NAME")
 
 	connStr := go_ora.BuildUrl(host, port, serviceName, user, password, nil)
 
